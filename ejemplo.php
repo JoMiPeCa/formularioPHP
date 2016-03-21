7ca9206113910bd9efd97941a6676f9d4dad9641
cetecom <cetecom@LC08L1328>
2016-03-21 20:02:02 -0300

    Ejemplo añadido

---
+++ ejemplo.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+
+
+
+
+switch ($variable) {
+    case $value:
+        codigo;
+        break;
+    case !$value:
+        codigo2;
+        break;
+
+    default:
+        codigo3;
+        break;
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
