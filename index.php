ae1e4e15171d4182080e0fbd7818162a5521a0ad
cetecom <cetecom@LC08L1328>
2016-03-21 19:27:52 -0300

    Index listo

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+        <form action="calculoAlgo.php" method="POST">
+            Valor A:<br>
+            <input type="text" name="val_a"><br>
+            Valor B:<br>
+            <input type="text" name="val_b">
+
+            <input type="submit" value="Enviar">
+        </form>
+
+
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+
+</html>
